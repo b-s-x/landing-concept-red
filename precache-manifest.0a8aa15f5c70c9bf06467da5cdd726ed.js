a1f83500bc18440d6046c34abd8c286f7678516d
b-s-x <b.s.x.mars@gmail.com>
2021-05-06 15:19:39 +0300

    Updates

---
+++ precache-manifest.0a8aa15f5c70c9bf06467da5cdd726ed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb41197067d78761a9849f3da885932",
+    "url": "/landing-concept-red/index.html"
+  },
+  {
+    "revision": "9a59a909a2f0486640e4",
+    "url": "/landing-concept-red/static/css/main.b63ff956.chunk.css"
+  },
+  {
+    "revision": "3a1ca6eec720af7213ba",
+    "url": "/landing-concept-red/static/js/2.cbdeb42e.chunk.js"
+  },
+  {
+    "revision": "9a59a909a2f0486640e4",
+    "url": "/landing-concept-red/static/js/main.41c383c6.chunk.js"
+  },
+  {
+    "revision": "dab6d0984a44064b5b56",
+    "url": "/landing-concept-red/static/js/runtime-main.7efafc4b.js"
+  },
+  {
+    "revision": "a7aadf3337b13c1d1811e42a8f2b6c66",
+    "url": "/landing-concept-red/static/media/austin.a7aadf33.webp"
+  },
+  {
+    "revision": "209dcd10075f7628d6cb393e72e4939c",
+    "url": "/landing-concept-red/static/media/beijing.209dcd10.webp"
+  },
+  {
+    "revision": "2bc381878816686be22cef7037544540",
+    "url": "/landing-concept-red/static/media/dallas.2bc38187.webp"
+  },
+  {
+    "revision": "ebb8196532e1924ad86530b2cde4c220",
+    "url": "/landing-concept-red/static/media/newyork.ebb81965.webp"
+  },
+  {
+    "revision": "b58ad59e1fd1f34d49ef692f3ff3ff8d",
+    "url": "/landing-concept-red/static/media/sanfrancisco.b58ad59e.webp"
+  }
+]);
